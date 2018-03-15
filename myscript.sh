1273394dada5dbdc3c44b8fbbafc2a4ab5360bb8
abdelrahman1992 <abdelrahman1_magdy1992@hotmail.com>
2018-03-15 14:00:04 +0200

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,15 @@
+app=$1
+chmod +x checkApp
+p1=$(./checkApp $app | cut -d "=" -f 2 | cut -d ")" -f1)
+p2=$(./pss | awk '{print $2}')
+
+echo $p1
+echo $p2
+
+if [[ p1 -eq p2 ]];
+ then
+    echo "./startApp"
+#./startApp";
+ else
+    echo "./startApp"
+fi
